70c1ffe4ef80ba4f6de89ccffb41a749b52308fc
sangwonlee06 <sangwonlee0622@outlook.com>
2026-02-08 06:48:35 -0800

    feat: send packet from arduino to stm32

--- firmware/arduino/arduino_test/arduino_test.ino
+++ firmware/arduino/arduino_test/arduino_test.ino
@@ -1,9 +1,13 @@
 #include <SoftwareSerial.h>
 #include "arduino_test_proc.h"
 #include "util_proto.h"
+#include "drv_hcsr04.h"
 
 #define TX_PIN        10
 #define RX_PIN        11
+
+#define TRIG_PIN      3
+#define ECHO_PIN      4
 
 SoftwareSerial g_mySerial(RX_PIN, TX_PIN);
 
@@ -27,20 +31,20 @@ void setup() {
   Serial.println("TEST Loop back");
 
   unsigned char ucData[4] = {0};
-        unsigned char ucEnqueueBuffer[256];
-        t_Protocol stTestProto;
+  unsigned char ucEnqueueBuffer[256];
+  t_Protocol stTestProto;
 
-        memset(ucEnqueueBuffer, 0, sizeof(ucEnqueueBuffer));
-        memset(&stTestProto, 0, sizeof(stTestProto));
+  memset(ucEnqueueBuffer, 0, sizeof(ucEnqueueBuffer));
+  memset(&stTestProto, 0, sizeof(stTestProto));
 
-        /* Fill protocol fields */
-        stTestProto.ucStart  = START_CODE;
-        stTestProto.usLength = (unsigned short)sizeof(ucData);
-        stTestProto.ucCmd    = CMD_REPLY_SENSOR_DATA;
-        memcpy(&stTestProto.ucData, ucData, 4);
-        stTestProto.ucEnd    = END_CODE;
+  /* Fill protocol fields */
+  stTestProto.ucStart  = START_CODE;
+  stTestProto.usLength = (unsigned short)sizeof(ucData);
+  stTestProto.ucCmd    = CMD_REPLY_SENSOR_DATA;
+  memcpy(&stTestProto.ucData, ucData, 4);
+  stTestProto.ucEnd    = END_CODE;
 
-        {
+  {
             PROTOCOL_RET ret =  util_proto_MakePacket(&stTestProto,
                                                      ucEnqueueBuffer,
                                                      (unsigned short)sizeof(ucEnqueueBuffer));
@@ -54,21 +58,30 @@ void setup() {
             }
             else
             {
-                /* Frame length = header + payload + tail */
-                unsigned short usFrameLen =
-                    (unsigned short)(HEADER_SIZE + stTestProto.usLength + TAIL_SIZE);
+                /* Frame length = header + payload + tail == ret*/
+//                unsigned short usFrameLen =
+//                    (unsigned short)(HEADER_SIZE + stTestProto.usLength + TAIL_SIZE);
 
-                dumpHex(ucEnqueueBuffer, usFrameLen);
+                // dumpHex(ucEnqueueBuffer, ret);
 
                 // (void)ByteQueue_Write(BYTE_QUEUE_UART_ARD,
                 //                       ucEnqueueBuffer,
                 //                       usFrameLen);
+
+                g_mySerial.write(ucEnqueueBuffer, ret);
+                // Serial.write(ucEnqueueBuffer, ret);
+
             }
         }
+
+    drv_hcsr04_Init(TRIG_PIN, ECHO_PIN);
 }
 
 void loop() {
-  g_mySerial.write(0xAB);
+  // g_mySerial.write(0xAB);
+  drv_hcsr04_SendSonar();
+  float distance = drv_hcsr04_GetCmData();
+  // Serial.println(distance);
   
   delay(1000);
 
